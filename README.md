ac0ca96027d88fe782dfde4e30c2805df847d58b
Tyler Emel <temelaudio@gmail.com>
2024-01-17 01:00:59 -0800

    step-1: fetch plants

--- README.md
+++ README.md
@@ -1,16 +1,8 @@
-# planttracker_app
+# Indoor Plant Tracker App
 
-A new Flutter project.
+This application is a project started to help me track our houseplants and better care for them.
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+### planttracker_app
+![Static Badge](https://img.shields.io/badge/Dart-3.2.3-blue?logo=dart) 
+![Static Badge](https://img.shields.io/badge/Flutter-3.16.5-blue?logo=flutter)
+![Static Badge](https://img.shields.io/badge/Dio-5.4.0-blue)
--- lib/main.dart
+++ lib/main.dart
@@ -15,25 +15,100 @@ void main() {
   ));
 }
 
-final dio = Dio();
-String url = 'https://perenual.com/api/species-list?key=$plantKey&indoor=1';
+class PlantItem {
+  final String commonName;
+  final String scientificName;
+  final String watering;
+
+  PlantItem({
+    required this.commonName,
+    required this.scientificName,
+    required this.watering,
+  });
+}
 
 class HomePage extends HookWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final items = useState<List<PlantItem>>([]);
+    final loading = useState<bool>(false);
+    final error = useState<Object?>(null);
+    final page = useState<int>(1);
+
+    Future<void> fetchData() async {
+      try {
+        loading.value = true;
+        final dio = Dio();
+        final response = await dio.get(
+            'https://perenual.com/api/species-list?key=$plantKey&indoor=1&page=${page.value}');
+
+        devtools.log(response.data['data'].toString());
+
+        final parsedItems = response.data['data'] as List<dynamic>;
+
+        final itemsList = parsedItems.map((item) {
+          return PlantItem(
+            commonName: item['common_name'].toString(),
+            scientificName: item['scientific_name'].toString(),
+            watering: item['watering'].toString(),
+          );
+        }).toList();
+
+        items.value = itemsList;
+      } catch (e) {
+        error.value = e;
+      } finally {
+        loading.value = false;
+      }
+    }
+
     return Scaffold(
-        appBar: AppBar(title: const Text('Home Title')),
-        body: Column(
-          children: [
-            TextButton(
+      appBar: AppBar(title: const Text('Home Title')),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: fetchData,
+            child: const Text('Fetch Data'),
+          ),
+          if (loading.value)
+            const Center(child: CircularProgressIndicator())
+          else if (error.value != null)
+            Center(child: Text('Error: ${error.toString()}'))
+          else
+            Expanded(
+              child: ListView.builder(
+                itemCount: items.value.length,
+                itemBuilder: (context, index) {
+                  final plantItem = items.value[index];
+                  return ListTile(
+                    title: Text(plantItem.commonName),
+                    subtitle: Text(plantItem.scientificName),
+                    leading:
+                        Text(plantItem.watering), // Assuming a 'title' property
+                  );
+                },
+              ),
+            ),
+          ElevatedButton(
               onPressed: () {
-                devtools.log('Button Pressed');
+                if (page.value > 1) {
+                  page.value--;
+                  fetchData();
+                }
               },
-              child: const Text("Grab Indoor Plants"),
-            )
-          ],
-        ));
+              child: const Text('Get Last Page')),
+          ElevatedButton(
+              onPressed: () {
+                page.value++;
+                fetchData();
+              },
+              child: const Text('Get Next Page')),
+        ],
+      ),
+    );
   }
 }
+
+// 'https://perenual.com/api/species-list?key=$plantKey&indoor=1'
