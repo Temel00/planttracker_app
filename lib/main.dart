bdfd10ea4b1b7f0e5ad6b1846cdafd92559152f0
Tyler Emel <temelaudio@gmail.com>
2024-01-17 23:56:58 -0800

    step-2: firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,11 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:planttracker_app/auth/secrets.dart';
 import 'package:dio/dio.dart';
 import 'dart:developer' as devtools show log;
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
     title: 'Flutter Demo',
     theme: ThemeData(
@@ -66,49 +69,70 @@ class HomePage extends HookWidget {
 
     return Scaffold(
       appBar: AppBar(title: const Text('Home Title')),
-      body: Column(
-        children: [
-          ElevatedButton(
-            onPressed: fetchData,
-            child: const Text('Fetch Data'),
-          ),
-          if (loading.value)
-            const Center(child: CircularProgressIndicator())
-          else if (error.value != null)
-            Center(child: Text('Error: ${error.toString()}'))
-          else
-            Expanded(
-              child: ListView.builder(
-                itemCount: items.value.length,
-                itemBuilder: (context, index) {
-                  final plantItem = items.value[index];
-                  return ListTile(
-                    title: Text(plantItem.commonName),
-                    subtitle: Text(plantItem.scientificName),
-                    leading:
-                        Text(plantItem.watering), // Assuming a 'title' property
-                  );
-                },
-              ),
-            ),
-          ElevatedButton(
-              onPressed: () {
-                if (page.value > 1) {
-                  page.value--;
-                  fetchData();
-                }
-              },
-              child: const Text('Get Last Page')),
-          ElevatedButton(
-              onPressed: () {
-                page.value++;
-                fetchData();
-              },
-              child: const Text('Get Next Page')),
-        ],
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              return Column(
+                children: [
+                  ElevatedButton(
+                    onPressed: fetchData,
+                    child: const Text('Fetch Data'),
+                  ),
+                  if (loading.value)
+                    const Center(child: CircularProgressIndicator())
+                  else if (error.value != null)
+                    Center(child: Text('Error: ${error.toString()}'))
+                  else
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: items.value.length,
+                        itemBuilder: (context, index) {
+                          final plantItem = items.value[index];
+                          return ListTile(
+                            title: Text(plantItem.commonName),
+                            subtitle: Text(plantItem.scientificName),
+                            leading: Text(plantItem.watering),
+                            trailing: IconButton(
+                                icon: const Icon(Icons.add),
+                                onPressed: () {
+                                  // TODO: Implement add plant to myPlants page
+                                  devtools
+                                      .log('Adding a plant to myPlants page.');
+                                }), // Assuming a 'title' property
+                          );
+                        },
+                      ),
+                    ),
+                  ElevatedButton(
+                      onPressed: () {
+                        if (page.value > 1) {
+                          page.value--;
+                          fetchData();
+                        }
+                      },
+                      child: const Text('Get Last Page')),
+                  ElevatedButton(
+                      onPressed: () {
+                        page.value++;
+                        fetchData();
+                      },
+                      child: const Text('Get Next Page')),
+                ],
+              );
+            default:
+              return const CircularProgressIndicator();
+          }
+        },
       ),
     );
   }
 }
 
-// 'https://perenual.com/api/species-list?key=$plantKey&indoor=1'
+// TODO: Add Register and Login Views
+// TODO: Separate App Initialization from Login logic
+// TODO: Create MyPlants page to store user's plants
+// TODO: Add a custom_name to PlantItem
+// TODO: Protect main UI from unathenticated Users
