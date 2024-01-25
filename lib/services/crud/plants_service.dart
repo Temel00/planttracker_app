b0c58c73006bbfc073f1149dbd33e355c1a4d480
Tyler Emel <temelaudio@gmail.com>
2024-01-25 00:47:25 -0800

    step-8: Local User and Plant CRUD

---
+++ lib/services/crud/plants_service.dart
@@ -0,0 +1,268 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' show join;
+import 'package:planttracker_app/services/crud/crud_exceptions.dart';
+
+class PlantsService {
+  Database? _db;
+
+  Future<DatabasePlant> updatePlant({
+    required DatabasePlant plant,
+    required String text,
+  }) async {
+    final db = _getDatabaseOrThrow();
+
+    await getPlant(id: plant.id);
+
+    final updatesCount = await db.update(
+      plantTable,
+      {
+        textColumn: text,
+        isSyncedWithCloudColumn: 0,
+      },
+      where: 'id = ?',
+      whereArgs: [plant.id],
+    );
+
+    if (updatesCount == 0) {
+      throw CouldNotUpdatePlantException();
+    } else {
+      return await getPlant(id: plant.id);
+    }
+  }
+
+  Future<Iterable<DatabasePlant>> getAllPlants() async {
+    final db = _getDatabaseOrThrow();
+    final plants = await db.query(plantTable);
+
+    return plants.map((plantRow) => DatabasePlant.fromRow(plantRow));
+  }
+
+  Future<DatabasePlant> getPlant({required int id}) async {
+    final db = _getDatabaseOrThrow();
+    final plants = await db.query(
+      plantTable,
+      limit: 1,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+    if (plants.isEmpty) {
+      throw PlantNotFoundException();
+    } else {
+      return DatabasePlant.fromRow(plants.first);
+    }
+  }
+
+  Future<int> deleteAllPlants() async {
+    final db = _getDatabaseOrThrow();
+    return await db.delete(plantTable);
+  }
+
+  Future<void> deletePlant({required int id}) async {
+    final db = _getDatabaseOrThrow();
+    final deletedCount = await db.delete(
+      plantTable,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+    if (deletedCount != 1) {
+      throw CouldNotDeletePlantException();
+    }
+  }
+
+  Future<DatabasePlant> createPlant({required DatabaseUser owner}) async {
+    final db = _getDatabaseOrThrow();
+
+    // make suer owner exists in database with correct id
+    final dbUser = await getUser(email: owner.email);
+    if (dbUser != owner) {
+      throw UserNotFoundException();
+    }
+
+    const text = '';
+    // create the plant
+    final plantId = await db.insert(plantTable, {
+      userIdColumn: owner.id,
+      textColumn: text,
+      isSyncedWithCloudColumn: 1,
+    });
+
+    final plant = DatabasePlant(
+      id: plantId,
+      userId: owner.id,
+      text: text,
+      isSyncedWithCloud: true,
+    );
+
+    return plant;
+  }
+
+  Future<DatabaseUser> getUser({required String email}) async {
+    final db = _getDatabaseOrThrow();
+    final results = await db.query(
+      userTable,
+      limit: 1,
+      where: 'email = ?',
+      whereArgs: [email.toLowerCase()],
+    );
+
+    if (results.isEmpty) {
+      throw UserNotFoundException();
+    } else {
+      return DatabaseUser.fromRow(results.first);
+    }
+  }
+
+  Future<DatabaseUser> createUser({required String email}) async {
+    final db = _getDatabaseOrThrow();
+    final results = await db.query(
+      userTable,
+      limit: 1,
+      where: 'email = ?',
+      whereArgs: [email.toLowerCase()],
+    );
+    if (results.isNotEmpty) {
+      throw UserAlreadyExistsException();
+    }
+
+    final userId = await db.insert(userTable, {
+      emailColumn: email.toLowerCase(),
+    });
+
+    return DatabaseUser(
+      id: userId,
+      email: email,
+    );
+  }
+
+  Future<void> deleteUser({required String email}) async {
+    final db = _getDatabaseOrThrow();
+    final deletedCount = await db.delete(
+      userTable,
+      where: 'email = ?',
+      whereArgs: [email.toLowerCase()],
+    );
+    if (deletedCount != 1) {
+      throw CouldNotDeleteUserException();
+    }
+  }
+
+  Database _getDatabaseOrThrow() {
+    final db = _db;
+    if (db == null) {
+      throw DatabaseIsNotOpenException();
+    } else {
+      return db;
+    }
+  }
+
+  Future<void> close() async {
+    final db = _db;
+    if (db == null) {
+      throw DatabaseIsNotOpenException();
+    } else {
+      await db.close();
+      _db = null;
+    }
+  }
+
+  Future<void> open() async {
+    if (_db != null) {
+      throw DatabaseAlreadyOpenException();
+    }
+    try {
+      final docsPath = await getApplicationDocumentsDirectory();
+      final dbPath = join(docsPath.path, dbName);
+      final db = await openDatabase(dbPath);
+      _db = db;
+
+      // create user table
+      await db.execute(createUserTable);
+      // create plant table
+      await db.execute(createPlantTable);
+    } on MissingPlatformDirectoryException {
+      throw UnableToGetDocumentsDirectoryException();
+    }
+  }
+}
+
+@immutable
+class DatabaseUser {
+  final int id;
+  final String email;
+
+  const DatabaseUser({
+    required this.id,
+    required this.email,
+  });
+
+  DatabaseUser.fromRow(Map<String, Object?> map)
+      : id = map[idColumn] as int,
+        email = map[emailColumn] as String;
+
+  @override
+  String toString() => 'Person, ID = $id, email = $email';
+
+  @override
+  bool operator ==(covariant DatabaseUser other) => id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
+}
+
+class DatabasePlant {
+  final int id;
+  final int userId;
+  final String text;
+  final bool isSyncedWithCloud;
+
+  const DatabasePlant({
+    required this.id,
+    required this.userId,
+    required this.text,
+    required this.isSyncedWithCloud,
+  });
+
+  DatabasePlant.fromRow(Map<String, Object?> map)
+      : id = map[idColumn] as int,
+        userId = map[userIdColumn] as int,
+        text = map[textColumn] as String,
+        isSyncedWithCloud =
+            (map[isSyncedWithCloudColumn] as int) == 1 ? true : false;
+
+  @override
+  String toString() =>
+      'Plant: id = $id, userId = $userId, isSyncedWithCloud = $isSyncedWithCloud, text = $text';
+
+  @override
+  bool operator ==(covariant DatabasePlant other) => id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
+}
+
+const dbName = 'plants.db';
+const plantTable = 'plant';
+const userTable = 'user';
+const idColumn = 'id';
+const emailColumn = 'email';
+const userIdColumn = 'user_id';
+const textColumn = 'text';
+const isSyncedWithCloudColumn = 'is_synced_with_cloud';
+//Create user table
+const createUserTable = '''CREATE TABLE IF NOT EXISTS "user" (
+  "id"	INTEGER NOT NULL,
+  "email"	TEXT NOT NULL UNIQUE,
+  PRIMARY KEY("id")
+);''';
+
+// Create plant table
+const createPlantTable = '''CREATE TABLE IF NOT EXISTS "plant" (
+  "id"	INTEGER NOT NULL,
+  "user_id"	INTEGER,
+  "text"	TEXT,
+  "is_synced_with_cloud"	INTEGER NOT NULL DEFAULT 0,
+  FOREIGN KEY("user_id") REFERENCES "user"("id"),
+  PRIMARY KEY("id" AUTOINCREMENT)
+);''';
